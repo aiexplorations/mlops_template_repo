97b05335c7996871b5a98336f33dfde71c08811f
aiexplorations <rexplorations@gmail.com>
2022-04-30 14:02:05 +0530

    modified gen data to accept a file name argument

--- src/gen_data.py
+++ src/gen_data.py
@@ -4,7 +4,7 @@ import logging
 
 n = 1000
 
-def gen_data(n):
+def gen_data(n, file_name):
     if n >= 1:
         non_zero_neg = n >= 1
        
@@ -13,8 +13,7 @@ def gen_data(n):
             df = pd.DataFrame({"x1":np.random.normal(10,1,n), "x2": np.random.normal(20,2,n)})
             df['y'] = np.random.normal(0,1,n) + 2*df['x1'] + 3*df['x2']
 
-
-            df.to_csv("data.csv")
+            df.to_csv(file_name)
 
             logging.log(logging.INFO,'Created data.csv with '+str(n)+' records,')
             
@@ -24,4 +23,4 @@ def gen_data(n):
         logging.error(logging.ERROR, "n should be greater than or equal to 1")
         raise Exception("n should be greater than or equal to 1")
 
-gen_data(n)
+gen_data(n, "data.csv")
